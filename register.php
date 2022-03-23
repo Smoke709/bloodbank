67163d5a84640911e7c82363602eaa8d50d0c1f6
Smoke709 <susanmaharjan6@gmail.com>
2022-03-23 12:52:54 +0545

    location

--- register.php
+++ register.php
@@ -1,80 +1,77 @@
-<?php 
+<?php
 session_start();
 if (isset($_SESSION['hid'])) {
   header("location:bloodrequest.php");
-}elseif (isset($_SESSION['rid'])) {
+} elseif (isset($_SESSION['rid'])) {
   header("location:sentrequest.php");
-}else{
+} else {
 ?>
-<!DOCTYPE html>
-<html>
-<?php $title="Bloodbank | Register"; ?>
-<?php require 'head.php'; ?>
-<body>
-  <?php include 'header.php'; ?>
+  <!DOCTYPE html>
+  <html>
+  <?php $title = "Bloodbank | Register"; ?>
+  <?php require 'head.php'; ?>
 
+  <body>
+
+    <head>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap');
+
+        .hHeader {
+          color: green;
+          font-family: 'Ubuntu', sans-serif;
+        }
+      </style>
+    </head>
+    <?php include 'header.php'; ?>
     <div class="container cont">
 
-    <?php require 'message.php'; ?>
-
+      <?php require 'message.php'; ?>
       <div class="row justify-content-center">
-        <div class="col-lg-4 col-md-5 col-sm-6 col-xs-7 mb-5">
-          <div class="card rounded">
-            <ul class="nav nav-tabs justify-content-center bg-light" style="padding: 20px">
-              <li class="nav-item">
-                <a class="nav-link active" data-toggle="tab" href="#hospitals">Hospitals</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#receivers">Receivers</a>
-              </li>
-            </ul>
-
-    <div class="tab-content">
-
-       <div class="tab-pane container active" id="hospitals">
-
-        <form action="file/hospitalReg.php" method="post" enctype="multipart/form-data">
-          <input type="text" name="hname" placeholder="Hospital Name" class="form-control mb-3" required>
-          <input type="text" name="hcity" placeholder="Hospital City" class="form-control mb-3" required>
-          <input type="tel" name="hphone" placeholder="Hospital Phone Number" class="form-control mb-3" required pattern="[0,6-9]{1}[0-9]{9,11}" title="Password must have start from 0,6,7,8 or 9 and must have 10 to 12 digit">
-          <input type="email" name="hemail" placeholder="Hospital Email" class="form-control mb-3" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
-          <input type="password" name="hpassword" placeholder="Hospital Password" class="form-control mb-3" required minlength="6">
-          <input type="submit" name="hregister" value="Register" class="btn btn-primary btn-block mb-4">
-        </form>
-
-       </div>
+        <div class="col-lg-6 col-md-5 col-sm-6 col-xs-7 mb-5">
 
 
-       <div class="tab-pane container fade" id="receivers">
+          <div class="hHeader" style="text-align: center;">
+            <h2>Hospital Registration</h2>
+          </div>
+          <form action="file/hospitalReg.php" method="post" enctype="multipart/form-data">
+            <input type="text" name="hname" placeholder="Hospital Name" class="form-control mb-3" required>
+            <input type="text" name="hcity" placeholder="Hospital City" class="form-control mb-3" required>
+            <input type="tel" name="hphone" placeholder="Hospital Phone Number" class="form-control mb-3" required pattern="[0,6-9]{1}[0-9]{9,11}" title="Password must have start from 0,6,7,8 or 9 and must have 10 to 12 digit">
+            <input type="email" name="hemail" placeholder="Hospital Email" class="form-control mb-3" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+            <input type="password" name="hpassword" placeholder="Hospital Password" class="form-control mb-3" required minlength="6">
 
-         <form action="file/receiverReg.php" method="post" enctype="multipart/form-data">
-          <input type="text" name="rname" placeholder="Receiver Name" class="form-control mb-3" required>
-          <select name="rbg" class="form-control mb-3" required>
-                <option disabled="" selected="">Blood Group</option>
-                <option value="A+">A+</option>
-                <option value="A-">A-</option>
-                <option value="B+">B+</option>
-                <option value="B-">B-</option>
-                <option value="AB+">AB+</option>
-                <option value="AB-">AB-</option>
-                <option value="O+">O+</option>
-                <option value="O-">O-</option>
-          </select>
-          <input type="text" name="rcity" placeholder="Receiver City" class="form-control mb-3" required>
-          <input type="tel" name="rphone" placeholder="Receiver Phone Number" class="form-control mb-3" required pattern="^\d{10}$" title="ContactNo. must have 10 digit">
-          <input type="email" name="remail" placeholder="Receiver Email" class="form-control mb-3" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
-          <input type="password" name="rpassword" placeholder="Receiver Password" class="form-control mb-3" required minlength="6">
-          <input type="submit" name="rregister" value="Register" class="btn btn-primary btn-block mb-4">
-        </form>
+            <?php require 'map.php' ?>
+            <input type="submit" name="hregister" value="Register" class="btn btn-primary btn-block mb-4" style="margin-top: 20px;">
+            <div class="text-center"><a href="login.php" title="Click here">Already have account?</a></div>
+          </form>
 
-       </div>
+
+
+        </div>
+      </div>
     </div>
-    <a href="login.php" class="text-center mb-4" title="Click here">Already have account?</a>
-</div>
-</div>
-</div>
-</div>
-<?php require 'footer.php' ?>
-</body>
-</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <hr style="margin-bottom: 0;">
+    <?php require 'footer.php' ?>
+  </body>
+
+  </html>
 <?php } ?>
